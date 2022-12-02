a61a43e963f9f6c2171acda91c7bc0bcf9aea2e5
amr1208 <zjac364@live.rhul.ac.uk>
2022-12-02 16:57:31 +0000

    Junit Test for development of NumStack class using TDD. First Test
    initiates numStack object

---
+++ calculatorCoursework/src/test/java/uk/ac/rhul/cs2800/CalculatorNumStackTest.java
@@ -0,0 +1,18 @@
+package uk.ac.rhul.cs2800;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculatorNumStackTest {
+	private NumStack numS;
+	
+	@BeforeEach
+	void test() {
+		numS = new NumStack();
+	}
+
+
+
+}
