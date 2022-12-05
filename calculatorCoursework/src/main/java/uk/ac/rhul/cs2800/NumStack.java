be9647d6a9d033f22aaa8daba5c6802484c89f48
amr1208 <zjac364@live.rhul.ac.uk>
2022-12-05 16:50:04 +0000

    Refactored verison of both NumStack and the test file so files now
    conform with googlePlus checkstyle.

--- calculatorCoursework/src/main/java/uk/ac/rhul/cs2800/NumStack.java
+++ calculatorCoursework/src/main/java/uk/ac/rhul/cs2800/NumStack.java
@@ -2,33 +2,55 @@ package uk.ac.rhul.cs2800;
 
 import java.util.EmptyStackException;
 
-
+/**
+ * This class provides a facade for the numStack stack.
+ *
+ * @author ayaan
+ *
+ */
 public class NumStack {
-	private Stack numStack;
+  private Stack numStack;
 
-	public NumStack() {
-		numStack = new Stack();
-	}
+  /**
+   * Initiate Numstack object using stack.
+   */
+  public NumStack() {
+    numStack = new Stack();
+  }
 
-	public void push(float i) {
-		Entry number = new Entry(i);
-		numStack.push(number);
+  /**
+   *Method faciliates floats to be pushed onto the stack.
+   *
+   * @param i is a float 
+   */
+  public void push(float i) {
+    Entry number = new Entry(i);
+    numStack.push(number);
 
-	}
+  }
 
-	public float numPop() {
-		if (numStack.size == 0) {
-			throw new EmptyStackException();
-		}
-		float result = numStack.pop().getValue();
-		return result;
-	}
-	
+  /**
+   * Pops off the item on the top of the stack and returns the float.
+   *
+   * @return item at the top of stack
+   */
+  public float numPop() {
+    if (numStack.size == 0) {
+      throw new EmptyStackException();
+    }
+    float result = numStack.pop().getValue();
+    return result;
+  }
 
-	public boolean isEmpty() {
-		if (numStack.size == 0) {
-			return true;		
-		}
-		return false;
-	}
+  /**
+   * Method to check whether the stack is empty.
+   *
+   * @return boolean value representing whether stack has entries or not.
+   */
+  public boolean isEmpty() {
+    if (numStack.size == 0) {
+      return true;
+    }
+    return false;
+  }
 }
