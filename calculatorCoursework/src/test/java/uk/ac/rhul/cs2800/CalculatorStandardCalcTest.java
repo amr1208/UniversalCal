a0ae6467811a02fc5e25f0040179d4e140821120
amr1208 <zjac364@live.rhul.ac.uk>
2022-12-09 04:42:01 +0000

    StandardCalc developed using TDD with tests to check boundary cases
    and whether priority method works as expected.

--- calculatorCoursework/src/test/java/uk/ac/rhul/cs2800/CalculatorStandardCalcTest.java
+++ calculatorCoursework/src/test/java/uk/ac/rhul/cs2800/CalculatorStandardCalcTest.java
@@ -18,7 +18,7 @@ class CalculatorStandardCalcTest {
     assertEquals((!((sc.operatorRank("+")) <= (sc.operatorRankSym(Symbol.LEFT_BRACKET)))), true,
         "Testing whether symbol rank is functional");
   }
-  
+
   // test2
   // Test to see whether simple equation with one operator can be evaluated.
   @Test
@@ -30,25 +30,40 @@ class CalculatorStandardCalcTest {
 
   // test3
   // Test to see whether string with one set of brackets could be evaluated properly.
+  // Test first was aimed to see whether String was being read in and then outputted
+  // correctly and then moved towards solving a simple equation.
   @Test
   void testCalc() {
     StandardCalc sc = new StandardCalc();
     assertEquals(sc.evaluateInfix("( 5 * 6 + 7 ) - 2"), 35,
         "Evaluation of whether simple one bracket equation can be solved.");
   }
-  
+
   // test4
   // Test to see whether string with no operator runs.
   // test does run and pop's value at top of stack, so 5 in this case
   @Test
   void testnoOpCalc() {
     StandardCalc sc = new StandardCalc();
-    assertEquals(sc.evaluateInfix("4 5"), 5,
-        "Evaluation to test program doesn't crash");
+    assertEquals(sc.evaluateInfix("4 5"), 5, "Evaluation to test program doesn't crash");
   }
 
+  // test5
+  // Test to see whether string with just zeros crashes the program.
+  // it will not as method pops stack value
+  @Test
+  void testZero() {
+    StandardCalc sc = new StandardCalc();
+    assertEquals(sc.evaluateInfix("0 0"), 0, "Test to see if 0 causes an error ");
+  }
 
-
-
-
+  // test6
+  // test to see whether high priority operations execute as expected with others
+  @Test
+  void testCalcDiv() {
+    StandardCalc name = new StandardCalc();
+    assertEquals(name.evaluateInfix("( 5 * 6 + 6 ) / 2"), 18, "Complex equation test");
+  }
 }
+
+
