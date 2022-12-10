4301e9025eeb46a9b60600d74b3bcd8ebfd08d57
amr1208 <zjac364@live.rhul.ac.uk>
2022-12-10 23:05:33 +0000

    Update to Driver class and visual adjustments to GUI of Calculator.

--- calculatorCoursework/src/main/java/uk/ac/rhul/cs2800/Driver.java
+++ calculatorCoursework/src/main/java/uk/ac/rhul/cs2800/Driver.java
@@ -7,18 +7,25 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+/**
+ *Main application class for program.
+ *
+ * @author ayaan
+ */
 public class Driver extends Application {
 
+  /**
+   *Method launches javafx jar.
+   *
+   * @param args passed through stage
+   */
   public static void main(String[] args) {
     launch(args);
   }
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    //Parent root = FXMLLoader.load(getClass().getResource("/myView.fxml"));
     FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("myView.fxml"));
-//    loader.setController(this);
-//    loader.setRoot(this);
     Pane root = loader.load();
     Scene scene = new Scene(root, 800, 500);
     primaryStage.setScene(scene);
